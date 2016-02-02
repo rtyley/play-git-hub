7181b71c79c73530725b9f68755f613a2e1540a6
Roberto Tyley <roberto.tyley@theguardian.com>
2016-02-02 16:24:57 +0000

    Actually just get the non-optional ratelimit data

--- src/main/scala/com/madgag/scalagithub/GitHub.scala
+++ src/main/scala/com/madgag/scalagithub/GitHub.scala
@@ -205,11 +205,11 @@ object GitHub {
 class GitHub(ghCredentials: GitHubCredentials) {
   import GitHub._
 
-  def checkRateLimit()(implicit ec: EC): Future[ResponseMeta] = {
+  def checkRateLimit()(implicit ec: EC): Future[RateLimit] = {
     // GET /rate_limit  https://developer.github.com/v3/rate_limit/
     val url = apiUrlBuilder.addPathSegment("rate_limit").build()
 
-    ghCredentials.okHttpClient.execute(addAuth(new Builder().url(url).get).build()).map(ResponseMeta.from)
+    ghCredentials.okHttpClient.execute(addAuth(new Builder().url(url).get).build()).map(ResponseMeta.rateLimitFrom)
   }
 
   /**
