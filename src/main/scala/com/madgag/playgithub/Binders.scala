ab78d9ca77b8c33f21d76709c41de2ec7d9272d2
Roberto Tyley <roberto.tyley@gmail.com>
2016-04-21 10:17:25 +0100

    Add routes Binders taken from submitGit
    In build.sbt:
    
    routesImport ++= Seq("com.madgag.scalagithub.model._","com.madgag.playgithub.Binders._")

---
+++ src/main/scala/com/madgag/playgithub/Binders.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016 Roberto Tyley
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.madgag.playgithub
+
+import com.madgag.scalagithub.model.{PullRequestId, RepoId}
+import org.eclipse.jgit.lib.ObjectId
+import play.api.mvc.PathBindable.Parsing
+
+object Binders {
+
+  implicit object bindableObjectId extends Parsing[ObjectId](
+    ObjectId.fromString, _.name, (key: String, e: Exception) => s"Cannot parse parameter '$key' as a commit id: ${e.getMessage}"
+  )
+
+  /*
+   * e.g. GET  /api/$repo<[^/]+/[^/]+>/message-lookup  @controllers.Api.messageLookup(repo: RepoId)
+   */
+  implicit object bindableRepoId extends Parsing[RepoId](
+    RepoId.from, _.fullName, (key: String, e: Exception) => s"Cannot parse repo name '$key'"
+  )
+
+  /*
+   * e.g.GET  /$pr<[^/]+/[^/]+/pull/[\d]+>  controllers.Application.reviewPullRequest(pr: PullRequestId)
+   */
+  implicit object bindablePullRequestId extends Parsing[PullRequestId](
+    PullRequestId.from, _.slug, (key: String, e: Exception) => s"Cannot parse pull request '$key'"
+  )
+}
