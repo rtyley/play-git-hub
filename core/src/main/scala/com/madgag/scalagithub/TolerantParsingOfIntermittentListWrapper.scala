6efd2e8ceb0e7a5b811993479c9446a6ec3c8e9f
Roberto Tyley <roberto.tyley@gmail.com>
2025-08-16 16:37:49 +0100

    Tolerate intermittent List wrappers used in JSON responses from the GitHub API
    Surprisingly the GitHub API sometimes, for a short period of time, will return
    a response with a surplus List wrapper (`[]`) around responses that are always
    supposed to return a single entity - I've observed this particularly for the
    'Get a reference' endpoint:
    
    https://docs.github.com/en/rest/git/refs?apiVersion=2022-11-28#get-a-reference
    
    ```
    GET /repos/{owner}/{repo}/git/ref/{ref}
    ```
    
    So for a short time the response returned will start like this:
    
    ```
    [{"ref":"refs/heads/feature-1", ... }]
    ```
    
    ...and then will settle on the expected response, which is identical except for
    the removed list wrapper:
    
    ```
    {"ref":"refs/heads/feature-1", ... }
    ```
    
    I've observed this happening when the entity has been recently created - if you
    repeatedly make the request, after a few seconds the response settles to the
    normal response format, without the list-wrapper.
    
    One way to handle this is simply performing retries, but it is also possible to
    tweak the parser to discard the outer wrapper if initial parsing fails - and then
    just parse the single element contained in the list. That's what this change does.

---
+++ core/src/main/scala/com/madgag/scalagithub/TolerantParsingOfIntermittentListWrapper.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2025 Roberto Tyley
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.madgag.scalagithub
+
+import play.api.libs.json.*
+
+object TolerantParsingOfIntermittentListWrapper {
+
+  val ListWithoutSingleEntryError = JsonValidationError("wrapper.list.does.not.contain.a.single.entry")
+
+  def tolerantlyParse[T : Reads](json: JsValue): JsResult[T] = {
+    json.validate[T].recoverWith { jsError =>
+      json.validate[Seq[T]].collect(ListWithoutSingleEntryError) {
+        case singleEntry :: Nil => singleEntry
+      }.recoverWith(_ => jsError)
+    }
+  }
+}
