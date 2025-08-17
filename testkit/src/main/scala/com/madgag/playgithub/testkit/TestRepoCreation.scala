a906e1ed8dc40f74d09d04765319550299daa8ed
Roberto Tyley <roberto.tyley@gmail.com>
2025-08-17 15:55:29 +0100

    Check that all branch refs on the test fixture PR actually are returned by the GitHub API

--- testkit/src/main/scala/com/madgag/playgithub/testkit/TestRepoCreation.scala
+++ testkit/src/main/scala/com/madgag/playgithub/testkit/TestRepoCreation.scala
@@ -27,6 +27,8 @@ import com.madgag.scalagithub.model.{Account, Repo}
 import com.madgag.time.Implicits._
 import org.apache.pekko.actor.ActorSystem
 import org.eclipse.jgit.api.{CloneCommand, Git}
+import org.eclipse.jgit.lib.Constants.R_HEADS
+import org.eclipse.jgit.lib.Ref
 import org.eclipse.jgit.transport.RemoteRefUpdate
 import org.scalatest.Inspectors.forAll
 import org.scalatest.concurrent.{Eventually, ScalaFutures}
@@ -86,6 +88,8 @@ trait TestRepoCreation extends Eventually with ScalaFutures {
       localGitRepo.git.branchCreate().setName(defaultBranchName).setStartPoint("HEAD").call()
     }
 
+    val branchRefs: Seq[Ref] = localGitRepo.getRefDatabase.getRefsByPrefix(R_HEADS).asScala.toSeq
+
     val creds = testFixturesCredentials().futureValue
     val pushResults =
       localGitRepo.git.push.setCredentialsProvider(creds.git).setPushTags().setPushAll().call()
@@ -96,6 +100,11 @@ trait TestRepoCreation extends Eventually with ScalaFutures {
 
     eventually {
       whenReady(testGithubRepo.refs.list().all()) { _ should not be empty }
+
+      forAll(branchRefs) { branchRef =>
+        val truncated = branchRef.getName.stripPrefix("refs/")
+        whenReady(testGithubRepo.refs.get(truncated))(_.result.objectId shouldBe branchRef.getObjectId)
+      }
     }
 
     val clonedRepo = eventually {
